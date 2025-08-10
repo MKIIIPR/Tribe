b0e56b3fb4d8e0c66e016378eef0f265588d3769
Beelze3 <kunst.wladislaw@outlook.de>
2025-08-10 20:28:15 +0200

    weg

---
+++ Tribe/Tribe/Controller/AntiforgeryController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Antiforgery;
+using Microsoft.AspNetCore.Mvc;
+
+[Route("[controller]")]
+[ApiController]
+public class AntiforgeryController : ControllerBase
+{
+    private readonly IAntiforgery _antiforgery;
+
+    public AntiforgeryController(IAntiforgery antiforgery)
+    {
+        _antiforgery = antiforgery;
+    }
+
+    [HttpGet("GetToken")]
+    public IActionResult GetToken()
+    {
+        var tokens = _antiforgery.GetAndStoreTokens(HttpContext);
+        return Ok(tokens.RequestToken); // Nur das Token senden
+    }
+}
